853de24802ff140447c36179fd0e6d687c50586f
SerhanTRGL <serhanturgul2@gmail.com>
2024-02-08 14:45:51 +0300

    updated update_emailnotificationdb.py

--- emailnotification/base/management/commands/update_emailnotificationdb.py
+++ emailnotification/base/management/commands/update_emailnotificationdb.py
@@ -1,16 +1,41 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
+from django.core.mail import send_mail
+from django.db.models import Q
 from base.models import EmailNotification
 from base.openprojectdb_models import WorkPackages
+import datetime
 
 class Command(BaseCommand):
+    
     help = 'This is a test command to see if it successfully adds a new row of data to the db'
-
-
+    
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument('--clearappdb', type=bool, help='Clear the application database')
+    
     def handle(self, *args, **options):
-        newData = EmailNotification(work_package_id=4,
-                                    mail_sent=True,
-                                    due_date='2025-05-05')
-        workpackages = WorkPackages.objects.using('openprojectdb').all()
-        for workpackage in workpackages:
-            print(workpackage)
-        newData.save()
+        clearAppDB = options['clearappdb']
+
+        if(clearAppDB):
+            EmailNotification.objects.all().delete()
+            print("Successfully cleared the app database")
+        else:
+            current_date = datetime.date.today()
+            workpackages = WorkPackages.objects.using('openprojectdb').all()
+            for workpackage in workpackages:
+                if(workpackage.due_date != None and current_date > workpackage.due_date):
+                    delta = current_date - workpackage.due_date
+        
+                    print(f"This task is overdue. Task id: {workpackage.id}. Overdue {delta.days} days.")
+            #send_mail("Hello from django ", "Hello world", "Django Custom Command", ["serhanturgul2@gmail.com"], fail_silently=False,)
+            self.updateAppDB()
+
+    def updateAppDB(self):
+        closed_status_id = 12
+        active_work_packages = WorkPackages.objects.using('openprojectdb').exclude(Q(status=closed_status_id) | Q(due_date__isnull=True))
+        for workpackage in active_work_packages:
+            if not EmailNotification.objects.filter(work_package_id=workpackage.id).exists():
+                EmailNotification.objects.create(work_package_id=workpackage.id, due_date=workpackage.due_date, mail_sent=False)
+            else:
+                print("Entry exists")
+    
+
--- emailnotification/emailnotification/settings.py
+++ emailnotification/emailnotification/settings.py
@@ -135,3 +135,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'carmanagementSystemMETUNCC@gmail.com'
+EMAIL_HOST_PASSWORD = 'tsan uyja hvbg fydf'
