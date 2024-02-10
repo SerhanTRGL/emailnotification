10275ca96ea332916290b364947b06211b235c59
SerhanTRGL <serhanturgul2@gmail.com>
2024-02-10 03:34:51 +0300

    updated update_emailnotificationdb.py

--- emailnotification/base/management/commands/update_emailnotificationdb.py
+++ emailnotification/base/management/commands/update_emailnotificationdb.py
@@ -3,39 +3,114 @@ from django.core.mail import send_mail
 from django.db.models import Q
 from base.models import EmailNotification
 from base.openprojectdb_models import WorkPackages
+from base.openprojectdb_models import Users
 import datetime
+from django.utils import timezone
+import time
 
 class Command(BaseCommand):
     
     help = 'This is a test command to see if it successfully adds a new row of data to the db'
+    closed_status_id = 12
     
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument('--clearappdb', type=bool, help='Clear the application database')
     
     def handle(self, *args, **options):
-        clearAppDB = options['clearappdb']
+        clear_app_db = options['clearappdb']
 
-        if(clearAppDB):
-            EmailNotification.objects.all().delete()
-            print("Successfully cleared the app database")
+        if clear_app_db:
+            self.clearAppDB()
         else:
-            current_date = datetime.date.today()
-            workpackages = WorkPackages.objects.using('openprojectdb').all()
-            for workpackage in workpackages:
-                if(workpackage.due_date != None and current_date > workpackage.due_date):
-                    delta = current_date - workpackage.due_date
+            #self.syncDatabases()
+            self.sendMails()
         
-                    print(f"This task is overdue. Task id: {workpackage.id}. Overdue {delta.days} days.")
-            #send_mail("Hello from django ", "Hello world", "Django Custom Command", ["serhanturgul2@gmail.com"], fail_silently=False,)
-            self.updateAppDB()
+    def syncDatabases(self):
+        start = time.time()
 
-    def updateAppDB(self):
-        closed_status_id = 12
-        active_work_packages = WorkPackages.objects.using('openprojectdb').exclude(Q(status=closed_status_id) | Q(due_date__isnull=True))
-        for workpackage in active_work_packages:
-            if not EmailNotification.objects.filter(work_package_id=workpackage.id).exists():
-                EmailNotification.objects.create(work_package_id=workpackage.id, due_date=workpackage.due_date, mail_sent=False)
+        current_date = datetime.date.today()
+        active_work_packages = WorkPackages.objects.using('openprojectdb').exclude(Q(status=self.closed_status_id) | Q(due_date__isnull=True))
+
+        #Loop below only adds new entries of packages that are not marked as closed, or updates the ones that are still
+        #not closed.
+        for work_package in active_work_packages:
+            if not EmailNotification.objects.filter(work_package_id=work_package.id).exists():
+                email_notification = EmailNotification(work_package_id=work_package.id, due_date=work_package.due_date)
+
+                #print(f"Work_package_id: {work_package.id} author_id: {work_package.author_id} responsible_id: {work_package.responsible_id} assigned_to_id: {work_package.assigned_to_id}")
+
+                if work_package.author_id != None:
+                    if Users.objects.using('openprojectdb').filter(Q(id=work_package.author_id) & Q(mail__isnull=False)).exists():
+                        email_notification.author_mail = Users.objects.using('openprojectdb').get(id=work_package.author_id).mail
+
+                if work_package.responsible_id != None:
+                    if Users.objects.using('openprojectdb').filter(Q(id=work_package.responsible_id) & Q(mail__isnull=False)).exists():
+                        email_notification.responsible_mail = Users.objects.using('openprojectdb').get(id=work_package.responsible_id).mail
+                
+                if work_package.assigned_to_id != None:
+                    if Users.objects.using('openprojectdb').filter(Q(id=work_package.assigned_to_id) & Q(mail__isnull=False)).exists():
+                        email_notification.assignee_mail = Users.objects.using('openprojectdb').get(id=work_package.assigned_to_id).mail
+                
+                email_notification.save()
+            else: # Work package already exists in app db
+                email_notification = EmailNotification.objects.get(work_package_id=work_package.id)
+                email_notification.due_date = work_package.due_date
+                email_notification.is_marked_as_closed = False #If a closed package can reach this line, it must be reopened
+
+                if work_package.author_id != None:
+                    if Users.objects.using('openprojectdb').filter(Q(id=work_package.author_id) & Q(mail__isnull=False)).exists():
+                        email_notification.author_mail = Users.objects.using('openprojectdb').get(id=work_package.author_id).mail
+
+                if work_package.responsible_id != None:
+                    if Users.objects.using('openprojectdb').filter(Q(id=work_package.responsible_id) & Q(mail__isnull=False)).exists():
+                        email_notification.responsible_mail = Users.objects.using('openprojectdb').get(id=work_package.responsible_id).mail
+                    
+                if work_package.assigned_to_id != None:
+                    if Users.objects.using('openprojectdb').filter(Q(id=work_package.assigned_to_id) & Q(mail__isnull=False)).exists():
+                        email_notification.assignee_mail = Users.objects.using('openprojectdb').get(id=work_package.assigned_to_id).mail
+
+                if email_notification.mail_sent and work_package.due_date >= current_date: #If mail is sent but due date is changed to a future date
+                    email_notification.mail_sent = False
+
+                email_notification.save()
+
+        email_notifications = EmailNotification.objects.all()
+
+        for email_notification in email_notifications:
+            if email_notification.is_marked_as_closed:
+                if WorkPackages.objects.using('openprojectdb').filter(id=email_notification.work_package_id).exists():
+                    work_package = WorkPackages.objects.using('openprojectdb').get(id=email_notification.work_package_id)
+                    if work_package.status_id != self.closed_status_id:
+                        email_notification.is_marked_as_closed = False
+                        email_notification.save()
             else:
-                print("Entry exists")
+                #Workpackage exists in app db but not in the openprojectdb, no need
+                #to send email in this case
+                if not WorkPackages.objects.using('openprojectdb').filter(id=email_notification.work_package_id).exists(): 
+                    email_notification.is_marked_as_closed = True 
+                    email_notification.save()
+
+        end = time.time()
+        print(f"Time elapsed syncing databases: {end-start} seconds.")
+
+    def sendMails(self):
+        current_date = datetime.date.today()
+        email_notifications = EmailNotification.objects.exclude(is_marked_as_closed=True).exclude(mail_sent=True)
+        
+        for email_notification in email_notifications:
+            if current_date > email_notification.due_date:
+                recipient_list = list(set([email_notification.assignee_mail, email_notification.author_mail, email_notification.responsible_mail]))
+                subject = f"{email_notification.work_package_id} Numaralı İş Paketinin Teslim Tarihi Geçti!"
+                print(subject)
+                message = f"{email_notification.work_package_id} Numaralı iş pakedinin teslim tarihi geçmiş bulunmakta\n Durum iş pakedinin sorumlularına ve yaratıcısına bildirilmiştir."
+                sender = "OpenProject Management"
+                send_mail(subject, message, sender, recipient_list, fail_silently=False)
+                email_notification.mail_sent = True
+                email_notification.mail_sent_date = timezone.now()
+                email_notification.save()
+
+    def clearAppDB(self):
+        EmailNotification.objects.all().delete()
+        print("Successfully cleared the app database")
     
 
--- emailnotification/base/models.py
+++ emailnotification/base/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class EmailNotification(models.Model):
     work_package_id = models.BigIntegerField()
     due_date = models.DateField()
-    mail_sent = models.BooleanField()
+    author_mail = models.CharField(default=None, null=True)
+    responsible_mail = models.CharField(default=None, null=True)
+    assignee_mail = models.CharField(default=None, null=True)
+    mail_sent = models.BooleanField(default=False)
+    mail_sent_date = models.DateTimeField(default=None, null=True)
+    is_marked_as_closed = models.BooleanField(default=False)
 
     class Meta:
         app_label = 'base'
